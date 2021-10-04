c75dd62d16fe693724f64469c9cf6ba497bf3d40
Donggeun Lee <piou987@gmail.com>
2021-10-04 16:54:09 +0900

    Add TestCase for /auth API

---
+++ tests/auth/test_auth.py
@@ -0,0 +1,99 @@
+"""
+
+"""
+import json
+
+def create_account(client, path, body):
+    return client.post(
+        path,
+        data=json.dumps(body),
+        content_type='application/json',
+        follow_redirects=True 
+    )
+def authenticate(client, body):
+    return client.post(
+        "/auth",
+        data=json.dumps(body),
+        content_type='application/json',
+        follow_redirects=True
+    )
+
+def test(client):
+    path = "/account/test001"
+    body = {
+        "password": "Z00keeper!2#",
+        "email": "worldskills.developer@gmail.com"
+    }
+    response = create_account(client, path, body)
+    assert response.status_code == 201
+
+    body = {
+        "username": "test001",
+        "password": "Z00keeper!2#"
+    }
+    response = authenticate(client, body)
+    assert response.status_code == 201
+
+def test_bad_request_username(client):
+    path = "/account/test001"
+    body = {
+        "password": "Z00keeper!2#",
+        "email": "worldskills.developer@gmail.com"
+    }
+    response = create_account(client, path, body)
+    assert response.status_code == 201
+
+    body = {
+        "no-username": "test001",
+        "password": "Z00keeper!2#"
+    }
+    response = authenticate(client, body)
+    assert response.status_code == 400
+
+def test_bad_request_password(client):
+    path = "/account/test001"
+    body = {
+        "password": "Z00keeper!2#",
+        "email": "worldskills.developer@gmail.com"
+    }
+    response = create_account(client, path, body)
+    assert response.status_code == 201
+
+    body = {
+        "username": "test001",
+        "no-password": "Z00keeper!2#"
+    }
+    response = authenticate(client, body)
+    assert response.status_code == 400
+
+def test_invalid_username(client):
+    path = "/account/test001"
+    body = {
+        "password": "Z00keeper!2#",
+        "email": "worldskills.developer@gmail.com"
+    }
+    response = create_account(client, path, body)
+    assert response.status_code == 201
+
+    body = {
+        "username": "nouser",
+        "password": "Z00keeper!2#"
+    }
+    response = authenticate(client, body)
+    assert response.status_code == 403
+
+def test_invalid_password(client):
+    path = "/account/test001"
+    body = {
+        "password": "Z00keeper!2#",
+        "email": "worldskills.developer@gmail.com"
+    }
+    response = create_account(client, path, body)
+    assert response.status_code == 201
+
+    body = {
+        "username": "test001",
+        "password": "nopassword"
+    }
+    response = authenticate(client, body)
+    assert response.status_code == 403
